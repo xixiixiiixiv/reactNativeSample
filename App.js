4ebf74ec43722ff8dc71fcae299cb82d71a39d84
xixiixiiixiv <xixiixiiixiv@gmail.com>
2018-07-24 23:03:54 +0900

    サンプルの書き込み https://qiita.com/YutamaKotaro/items/dd7846c6db15e2307daa

--- App.js
+++ App.js
@@ -39,16 +39,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#333',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
+    color: '#FFF',
     margin: 10,
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#FFF',
     marginBottom: 5,
   },
 });
